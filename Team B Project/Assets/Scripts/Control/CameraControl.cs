95e2c2f7f75f76b0afa900dadf0e7cd0533a38ad
Zachary Law <zachlaw90@gmail.com>
2021-02-08 14:36:42 -0500

    Iteration1 Partial Completion
    Camera panning with arrow keys, imp..

---
+++ Team B Project/Assets/Scripts/Control/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CameraControl : MonoBehaviour
+{
+    //This is used to control the camera panning.
+
+    [SerializeField]float speed = 2.0f;
+    float directionX = 0;
+    float directionZ = 0;
+    
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        Camera.main.transform.position += new Vector3(directionX, 0, directionZ) * speed * Time.deltaTime; 
+    }
+    private void OnPanHorizontal(InputValue value)
+    {
+        directionX = value.Get<float>();
+    }
+        private void OnPanVertical(InputValue value)
+    {
+        directionZ = value.Get<float>();
+    }
+}
