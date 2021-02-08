7d7ac29f9560015380d7f87d1e068bb0887274b3
jlag191 <47119131+jlag191@users.noreply.github.com>
2021-02-08 13:46:41 -0500

    Initial map setup

---
+++ Team B Project/Assets/Scripts/World/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public enum controlEnum { neutral, player1, player2 };
+    public controlEnum control;
+
+    private int curMetal;
+    public int maxMetal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void switchControl(controlEnum c)
+    {
+        control = c;
+    }
+}
