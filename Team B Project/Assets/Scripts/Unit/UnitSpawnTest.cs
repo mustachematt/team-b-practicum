ac470cea717db83842fec45281906211334eb588
Zachary Law <zachlaw90@gmail.com>
2021-02-08 13:43:48 -0500

    spawntest
    unit spawning

---
+++ Team B Project/Assets/Scripts/Unit/UnitSpawnTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UnitSpawnTest : MonoBehaviour
+{
+    GameObject[] waypoints;
+    [SerializeField] GameObject prefab;
+    
+    void Start()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("spawn");
+        Debug.Log("Hello" + waypoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnShip()
+    {
+        GameObject.Instantiate(prefab, waypoints[Random.Range(0,waypoints.Length)].transform);
+    }
+}
