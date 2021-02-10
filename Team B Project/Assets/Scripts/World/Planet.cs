9ea15b1aef0d4b17ccf132fe99546ea3a64bc10c
jlag191 <47119131+jlag191@users.noreply.github.com>
2021-02-10 13:51:30 -0500

    Began resource functionality of planets

--- Team B Project/Assets/Scripts/World/Planet.cs
+++ Team B Project/Assets/Scripts/World/Planet.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    /* 
+     * HOW PLANETS WORK:
+     * Each planet holds a state of which player has control of it,
+     * stored in an enum
+     * 
+     * A planet's resources aren't finite! If a planet's current (cur) value
+     * of a given resource is LESS than its maximum (max) value, after a few
+     * seconds, the planet will gain 1 more of that resource
+     * 
+     * Multiple ships can pull the same resource from the same planet at the
+     * same time
+     */
+
     public enum controlEnum { neutral, player1, player2 };
     public controlEnum control;
 
     private int curMetal;
     public int maxMetal;
 
+    // order of resources in both arrays have to be the same with current implementation
+    private int[] curResources;
+    private int[] maxResources;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        curResources = new int[] { curMetal };
+        maxResources = new int[] { maxMetal };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < maxResources.Length; ++i)
+            if (curResources[i] < maxResources[i])
+            {
+
+            }
     }
 
     void switchControl(controlEnum c)
