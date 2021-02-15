fb3b773f258a93a4e1d8c506c68ee8aa874d192e
Zachary Law <zachlaw90@gmail.com>
2021-02-15 14:02:16 -0500

    limit on camera bounds

--- Team B Project/Assets/Scripts/Control/CameraControl.cs
+++ Team B Project/Assets/Scripts/Control/CameraControl.cs
@@ -9,6 +9,8 @@ public class CameraControl : MonoBehaviour
     [SerializeField]float speed = 2.0f;
     float directionX = 0;
     float directionZ = 0;
+    [SerializeField] int xLimit = 32;
+    [SerializeField] int zLimit = 29;
     
     private void Start()
     {
@@ -16,7 +18,15 @@ public class CameraControl : MonoBehaviour
     }
     private void Update()
     {
-        Camera.main.transform.position += new Vector3(directionX, 0, directionZ) * speed * Time.deltaTime; 
+        {
+            var newPos = Camera.main.transform.position + (new Vector3(directionX, 0, directionZ) * speed * Time.deltaTime);
+            
+            if(newPos.x < xLimit && newPos.x > -xLimit 
+                && newPos.z < zLimit && newPos.z > -zLimit)
+            {
+                Camera.main.transform.position = newPos;
+            }
+        }   
     }
     private void OnPanHorizontal(InputValue value)
     {
