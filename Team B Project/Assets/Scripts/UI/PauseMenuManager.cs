8c72052495c1f511d92466e7bbe04d596f0ec29e
mmacuski <44308396+mmacuski@users.noreply.github.com>
2021-03-01 13:29:04 -0500

    Created pause menu script

---
+++ Team B Project/Assets/Scripts/UI/PauseMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    /* WHAT WILL BE ON THE PAUSE MENU
+
+        Volume Control
+            Sound Volume
+            Music Volume
+        
+        Controls?
+        Camera/Mouse/Control sensitivity
+        Quit/Return to main menu
+
+    */
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
