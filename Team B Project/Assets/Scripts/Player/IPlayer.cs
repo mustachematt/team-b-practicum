b7e0bf9b5ebc0183e5df7f88c6de09c28adaffdd
Jonathan Hansen <jhanse12@kent.edu>
2021-03-01 13:17:47 -0500

    Added Resource class
    Resource class added to player script

--- Team B Project/Assets/Scripts/Player/IPlayer.cs
+++ Team B Project/Assets/Scripts/Player/IPlayer.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public abstract class IPlayer : MonoBehaviour
 {
-    public int Resources { get; private set; } = 0;
+    public Dictionary<Planet.Resource.ResourceKind, Planet.Resource> Resources;
     private List<object> _ownedPlanets = new List<object>();
     public GameObject playerBase;
+    public GameObject waypoint;
 
     public IReadOnlyList<object> OwnedPlanets
     {
         get => _ownedPlanets.AsReadOnly();
     }
-    public void AddResources(int amount)
+    public void AddResources(Planet.Resource amount)
     {
-        Resources += amount;
+        Resources[amount.kind].currAmt += amount.currAmt;
     }
+
+    void Start()
+    {
+        Resources = new Dictionary<Planet.Resource.ResourceKind, Planet.Resource>();
+        Resources[Planet.Resource.ResourceKind.metal] = new Planet.Resource(0, Planet.Resource.ResourceKind.metal);
+        Resources[Planet.Resource.ResourceKind.fuel] = new Planet.Resource(0, Planet.Resource.ResourceKind.fuel);
+    }
+
     public virtual void Update()
     {
 
     }
+
     //Player Actions
-    public void SpawnUnit(StarShipUtilities.shipType unitType/*, GameObject waypoint*/)
+    public void SpawnUnit(StarShipUtilities.shipType unitType)
     {
         //Instantiate Ship Prefab, subtract resources
-        //resources -= shipPrefab.cost;
         GameObject shipPrefab = StarShipUtilities.Instance.ShipDictionary[unitType];
+        Resources[Planet.Resource.ResourceKind.metal].currAmt -= shipPrefab.GetComponent<StartShipScript>().price;
         GameObject ship = GameObject.Instantiate(shipPrefab, playerBase.transform.position, playerBase.transform.rotation);
-        //ship.StarShipScript.target = waypoint;
+        ship.GetComponent<StartShipScript>().target = waypoint;
     }
 
 }
