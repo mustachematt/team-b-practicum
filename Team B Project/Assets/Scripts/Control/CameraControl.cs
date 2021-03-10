26e84ff8b178611e4b80d3635ff840a382ed49cf
Zachary Law <zachlaw90@gmail.com>
2021-03-10 12:38:52 -0500

    working-on-edge-panning-shipPrefabs

--- Team B Project/Assets/Scripts/Control/CameraControl.cs
+++ Team B Project/Assets/Scripts/Control/CameraControl.cs
@@ -6,31 +6,57 @@ using UnityEngine.InputSystem;
 
 public class CameraControl : MonoBehaviour
 {
+    public float edgeClearance = 2.5f;
+    [SerializeField] float speed = 15f;
+    
+    [Header("Zoom Options")]
+    [Space]
     [SerializeField] float minCamSize = 10f;
     [SerializeField] float maxCamSize = 30f;
     [SerializeField] float zoomSpeed = 0.5f;
     //This is used to control the camera panning.
-    [SerializeField] float speed = 15f;
-    float directionX = 0;
-    float directionZ = 0;
+    
+    [Header("Screen  Size")]
+    [Space]
     [SerializeField] int xLimit = 32;
     [SerializeField] int zLimit = 29;
-
+    
+    private bool keyMoving;
+    private Vector3 edgeMove;
+    private float directionZ = 0;
+    private float directionX = 0;
     private void Update()
     {
         {
+            keyMoving = false;
+            //Vector3 mPOS = Input.mousePosition;
+
+            
             var pos = Camera.main.transform.position;
             var newPos = Camera.main.transform.position + (new Vector3(directionX, 0, directionZ) * speed * Time.deltaTime);
             
             if(newPos.x < xLimit && newPos.x > -xLimit)
             {
+                keyMoving = true;
                 Camera.main.transform.position = new Vector3(newPos.x, pos.y, pos.z);
             }
             pos = Camera.main.transform.position;
             if (newPos.z < zLimit && newPos.z > -zLimit)
             {
+                keyMoving = true;
                 Camera.main.transform.position = new Vector3(pos.x, pos.y, newPos.z);
             }
+            /*if(!keyMoving)
+            {
+                if(mPOS.x >= Screen.width - edgeClearance)
+                {
+                      edgeMove += Vector3.forward * speed * Time.deltaTime; 
+                }
+
+                Camera.main.transform.position = edgeMove;
+                
+            }
+            */
         }
         
     }
