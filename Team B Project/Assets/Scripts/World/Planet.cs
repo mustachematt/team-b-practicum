6eaa1934241b3b79088ee23f3ee979aa72b69887
cgrime11 <71897695+cgrime11@users.noreply.github.com>
2021-03-24 15:28:34 -0400

    added sprites to denoted the control of planets
    added sprites, player and enemy
    added a function in planet.cs to assign the correct sprite
    left in the test code to show functionality

--- Team B Project/Assets/Scripts/World/Planet.cs
+++ Team B Project/Assets/Scripts/World/Planet.cs
@@ -37,6 +37,9 @@ public class Planet : MonoBehaviour
     public enum controlEnum { neutral, player1, player2 };
     public controlEnum control;
 
+    private bool controlChanged=true;//controls when to change the sprite which shows ownership
+    private bool test = true;//used only for testing the control sprite
+
     // Resources
     public PlanetResource[] resources = new PlanetResource[2];
     private Dictionary<Resource.ResourceKind, Resource> _planetResourcesAsDictionary = new Dictionary<Resource.ResourceKind, Resource>();
@@ -76,11 +79,30 @@ public class Planet : MonoBehaviour
                 ++r.amount;
             }
         }
+        if (test == true)//just tests the functionality of control sprite
+        {
+            if(this.control == Planet.controlEnum.player1)
+                SwitchControl(Planet.controlEnum.player2);
+            else if (this.control == Planet.controlEnum.player2)
+                SwitchControl(Planet.controlEnum.player1);
+            test = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (controlChanged == true)
+        {
+            DisplayContoller();
+            controlChanged = false;
+        }
     }
 
     private void SwitchControl(controlEnum c)
     {
         control = c;
+        controlChanged = true;//added so the denotion of control can be changed only when control is changed
     }
 
     public Resource removeResources(Resource resourceToWithdraw)//removes resources from planet equally
@@ -98,5 +120,43 @@ public class Planet : MonoBehaviour
             }
         }
         return removed;
+    }
+
+    private void DisplayContoller() {
+        if (this.control != Planet.controlEnum.neutral)
+        {
+            if (this.control == Planet.controlEnum.player1)
+            {
+                if (this.transform.childCount !=0)//if the planet already had a sprite to denot control -> delete it to display the new one
+                {
+                    GameObject toDestroy=this.transform.GetChild(0).gameObject;
+                    Destroy(toDestroy);
+                }
+                var controlSprite = Resources.Load<Sprite>("playerDenotion");//load the correct sprite for this
+                GameObject child = new GameObject();//create a child to add the sprite to
+                SpriteRenderer renderer = child.AddComponent<SpriteRenderer>();
+                renderer.sprite = controlSprite;
+                child.transform.parent = this.transform;
+                child.transform.position = this.transform.position + new Vector3(0f, 0f, 8f);
+                child.transform.localScale = new Vector3(.5f, .5f, 0f);
+                child.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+            }
+            else if (this.control == Planet.controlEnum.player2)
+            {
+                if (this.transform.childCount !=0)//if the planet already had a sprite to denot control -> delete it to display the new one
+                {
+                    GameObject toDestroy = this.transform.GetChild(0).gameObject;
+                    Destroy(toDestroy);
+                }
+                var controlSprite = Resources.Load<Sprite>("enemyDenotion");//load the correect sprite for this
+                GameObject child = new GameObject();//create a child to add the sprite to
+                SpriteRenderer renderer = child.AddComponent<SpriteRenderer>();
+                renderer.sprite = controlSprite;
+                child.transform.parent = this.transform;
+                child.transform.position = this.transform.position + new Vector3(0f, 0f, 8f);
+                child.transform.localScale = new Vector3(.5f, .5f, 0f);
+                child.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+            }
+        }
     }
 }
