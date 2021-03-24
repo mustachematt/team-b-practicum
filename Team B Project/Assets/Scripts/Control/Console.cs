650edf7365bb4c9bcdd4503d76251a3342a7e5db
Kylemc1413 <kylet1412@gmail.com>
2021-03-24 14:39:27 -0400

    Initial functionality

---
+++ Team B Project/Assets/Scripts/Control/Console.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+public class Console : MonoBehaviour
+{
+    public static bool cheats = false;
+    InputField input;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        input = GetComponentInChildren<InputField>();
+    }
+    public void Submit()
+    {
+        string command = input.text;
+        input.text = "";
+        input.caretPosition = 0;
+      //  Debug.Log("Command: " + command);
+        CheckCommand(command);
+        gameObject.SetActive(false);
+    }
+    void CheckCommand(string command)
+    {
+        if (command.ToLower() == "sv_cheats 1")
+        {
+            Debug.Log("Cheats Activated");
+            cheats = true;
+        }
+        else if(command.ToLower() == "sv_cheats 0")
+        {
+            Debug.Log("Cheats Deactivated");
+            cheats = false;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Team B Project/Assets/Scripts/Control/ConsoleController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UI;
+public class ConsoleController : MonoBehaviour
+{
+    public GameObject console;
+    internal static Key[] konami = new Key[] { Key.UpArrow, Key.UpArrow, Key.DownArrow, Key.DownArrow, Key.LeftArrow, Key.RightArrow, Key.LeftArrow, Key.RightArrow, Key.B, Key.A };
+    private int konamiIndex = 0;
+    bool keyboard = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void CheckInput(Key key)
+    {
+       // Debug.Log(key);
+        if (Keyboard.current[konami[konamiIndex]].wasPressedThisFrame)
+            konamiIndex++;
+        else
+            konamiIndex = 0;
+        if (konamiIndex >= konami.Length)
+        {
+            konamiIndex = 0;
+            Konami();
+        }
+        if (key == Key.Backquote)
+        {
+            ActivateConsole();
+        }
+
+    }
+    public void ActivateConsole()
+    {
+        console.gameObject.SetActive(true);
+        console.GetComponentInChildren<UnityEngine.UI.InputField>().ActivateInputField();
+    }
+    public void Konami()
+    {
+        if (Console.cheats == false) return;
+        Debug.Log("Konami Achieved");
+        var transportCost = StarShipUtilities.Instance.ShipDictionary[Ship.shipType.Transport].price;
+        ControlledPlayer.Instance.AddResources(new Resource(transportCost * 100, Resource.ResourceKind.metal));
+        for (int i = 0; i < 100; i++)
+            ControlledPlayer.Instance.SpawnUnit(Ship.shipType.Transport);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Key key in Enum.GetValues(typeof(Key)))
+        {
+            if (key == Key.None || key == Key.IMESelected || key == Key.PrintScreen) continue;
+            if (Keyboard.current[key].wasPressedThisFrame)
+                CheckInput(key);
+        }
+
+    }
+}
