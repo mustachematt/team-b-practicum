e8810bc070545799102aeae175e79e11074dbf88
Kylemc1413 <kylet1412@gmail.com>
2021-04-28 14:16:31 -0400

    Update BuyMenuButton.cs

--- Team B Project/Assets/Scripts/UI/BuyMenuButton.cs
+++ Team B Project/Assets/Scripts/UI/BuyMenuButton.cs
@@ -17,4 +17,18 @@ public class BuyMenuButton : MonoBehaviour
     public int rangePts;
     public int fuelCost;
     public int metalCost;
+
+    public void Awake()
+    {
+        var ship = StarShipUtilities.Instance.ShipDictionary[this.ship];
+        var spriteRenderer = ship.gameObject.GetComponentInChildren<SpriteRenderer>();
+        shipImage = spriteRenderer.sprite;
+        attackPts = ship is AttackShip ? (ship as AttackShip).attackStrength.Value : 0;
+        defensePts = ship.armorStrength.Value;
+        moveSpeedPts = ship.maxSpeed.Value;
+        AtkSpeedPts = ship is AttackShip ? (ship as AttackShip).attackSpeed.Value : 0;
+        rangePts = ship is AttackShip ? (ship as AttackShip).attackRange.Value : 0;
+        fuelCost = (int)ship.price.fuel;
+        metalCost = (int)ship.price.metal;
+    }
 }
